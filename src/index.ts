4a96852ecca16983a005a0abdde35c463af687a7
Jimmy Bastos <jimmybastosdev@gmail.com>
2022-04-23 20:52:07 -0300

    🐛

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import supportMultipleEntry from './supportMultipleEntry';
 import getValidSettings from './getValidSettings';
 import { EntryParam } from './types/entry';
 
-function MultipleEntries(params: EntryParam[]) {
+function addMultipleEntries(params: EntryParam[]) {
   return supportMultipleEntry(getValidSettings(params));
 };
 
 
 
-module.exports = MultipleEntries;
+module.exports = addMultipleEntries;
